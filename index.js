15ded501d58e9143b569e31ebf8eeb092cced87c
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-08-14 11:05:56 +0100

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form");
+const formWrapper = document.querySelector(".form-wrapper");
 const userName = document.getElementById("name");
 const password = document.getElementById("password");
 const cPassword = document.getElementById("confirmPassword");
@@ -16,15 +16,14 @@ form.addEventListener("submit", (e) => {
 
 submitBtn.addEventListener("click", () => {
 
-    if(userName.value && (password.value !== cPassword.value)) {
+    // if(userName.value && (password.value !== cPassword.value)) {
 
-        passwordError.innerHTML = `<p style = "color: red">Passwords do not match.</p>`;
+    //     passwordError.innerHTML = `<p style = "color: red">Passwords do not match.</p>`;
 
-    } if(userName.value && (password.value == cPassword.value)) {
+    // } 
 
-        form.innerHTML = `<h5 class="announcement">Welcome back ${userName.value}!</h5>`;
-
-    }if (!userName.value && ((password.value == cPassword.value) || (password.value !== cPassword.value))){
+    
+    if (!userName.value && (!password.value || password.value)){
 
         nameError.innerHTML = "NOTE: Name is required";
 
@@ -32,13 +31,14 @@ submitBtn.addEventListener("click", () => {
 
         passwordError.innerHTML = `<p class="password-error-msg">Password must be longer than six characters.</p>`
 
-    } if((password.value && cPassword.value) && password.value.length > 10 ){
+    } if(password.value  && password.value.length > 10 ){
 
         passwordError.innerHTML = `<p class="password-error-msg">Password must not be longer than ten characters.</p>`
 
-    } if( password.value.toLowerCase() === "password" ){
+    }if(userName.value && password.value ) {
 
-        passwordError.innerHTML = `<p class="password-error-msg">Password cannot be ${password.value}.</p>`
+        formWrapper.innerHTML = `<h5 class="announcement">Login Successful!</h5>`;
+
     } 
     
 }); 
